e59a1c70b450af948c7b94061b23da502f45a126
mansieber <manfred.sieber@arkton.de>
2024-11-18 20:12:31 +0100

    Splashscreen added.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
+#include <QtDebug>
+
+#define VERSION "1.0.0"
+
+void delay(int millis);
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QApplication::setStyle("Windows");
+    QSplashScreen *splash = new QSplashScreen();
+    splash->setPixmap(QPixmap(":/images/splashimage.jpg"));
+    splash->show();
+    splash->showMessage(QString("Version ").append(VERSION), Qt::AlignRight | Qt::AlignBottom, Qt::black);
+    qDebug() << splash->message();
+    app.processEvents();
+    delay(3000);
     MainWindow w;
     w.show();
+    splash->finish(&w);
+    delete splash;
     return app.exec();
+}
+
+/*
+ * Function waits millis milliseconds.
+ */
+inline void delay(int millis)
+{
+    QEventLoop loop;
+    QTimer t;
+    t.connect(&t, &QTimer::timeout, &loop, &QEventLoop::quit);
+    t.start(millis);
+    loop.exec();
 }
